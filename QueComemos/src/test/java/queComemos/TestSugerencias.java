b833205225332e058e9a58e099704fd3f64b7021
matiasromero <matiromero.15@gmail.com>
2015-05-28 23:11:10 -0300

    test de sugerencia

--- QueComemos/src/test/java/queComemos/TestSugerencias.java
+++ QueComemos/src/test/java/queComemos/TestSugerencias.java
@@ -103,5 +103,105 @@ public class TestSugerencias {
 		
 		assertFalse(QueComemosApp.puedeSugerir(receta, usr));
 	}
+	
+	@Test
+	public void testPuedeSugerirCarneAlHornoAUnGrupoDeCarnivoros() {
+		
+		Set<String> comidasGusta1= new HashSet<String>();
+		comidasGusta1.add("Carne");
+		comidasGusta1.add("pollo");
+		Set<String> comidasNoGusta1= new HashSet<String>();
+		comidasNoGusta1.add("pescado");
+		PreferenciaAlimenticia preferencia1=new PreferenciaAlimenticia(comidasGusta1,comidasNoGusta1);
+		Usuario usuario1 = new Usuario("Pedro", "Masculino",
+				LocalDate.parse("1990-01-01"), 60.0, 1.7, "Leve",
+				preferencia1, null, null);
+		
+		Set<String> comidasGusta2= new HashSet<String>();
+		comidasGusta2.add("Carne");
+		comidasGusta2.add("pescado");
+		Set<String> comidasNoGusta2= new HashSet<String>();
+		comidasNoGusta2.add("cucaracha");
+		PreferenciaAlimenticia preferencia2=new PreferenciaAlimenticia(comidasGusta2,comidasNoGusta2);
+		Usuario usuario2 = new Usuario("Juan", "Masculino",
+				LocalDate.parse("1990-01-01"), 60.0, 1.7, "Leve",
+				preferencia2, null, null);
+		
+		Set<String> comidasGusta3= new HashSet<String>();
+		comidasGusta3.add("Carne");
+		Set<String> comidasNoGusta3= new HashSet<String>();
+		comidasNoGusta3.add("caracol");
+		PreferenciaAlimenticia preferencia3=new PreferenciaAlimenticia(comidasGusta3,comidasNoGusta3);
+		Usuario usuario3 = new Usuario("Pepo", "Masculino",
+				LocalDate.parse("1990-01-01"), 60.0, 1.7, "Leve",
+				preferencia3, null, null);
+		
+		Set<Usuario> usuariosColeccion = new HashSet<Usuario>();
+		usuariosColeccion.add(usuario1);
+		usuariosColeccion.add(usuario2);
+		usuariosColeccion.add(usuario3);
+		Set<String> comidasGusta= new HashSet<String>();
+		comidasGusta.add("Carne");
+		Set<String> comidasNoGusta= new HashSet<String>();
+		comidasNoGusta.add("lavandina");
+		PreferenciaAlimenticia preferencia=new PreferenciaAlimenticia(comidasGusta,comidasNoGusta);
+		Grupo grupo = new Grupo("UnNombre" , usuariosColeccion, preferencia);	
 
+		
+		assertTrue(grupo.puedeSugerir(receta));
+	}
+
+	
+	@Test
+	public void testPuedeSugerirCarneAlHornoAUnGrupoConUnoDiabetico() {
+		
+		Set<String> comidasProhibidas = new HashSet<String>();
+		comidasProhibidas.add("Azucar");
+		Set<CondicionPreexistenteI> condiciones = new HashSet<CondicionPreexistenteI>();
+		condiciones.add(new Diabetico("Diabetico", comidasProhibidas));
+		
+		Set<String> comidasGusta1= new HashSet<String>();
+		comidasGusta1.add("Carne");
+		comidasGusta1.add("pollo");
+		Set<String> comidasNoGusta1= new HashSet<String>();
+		comidasNoGusta1.add("pescado");
+		PreferenciaAlimenticia preferencia1=new PreferenciaAlimenticia(comidasGusta1,comidasNoGusta1);
+		Usuario usuario1 = new Usuario("Pedro", "Masculino",
+				LocalDate.parse("1990-01-01"), 60.0, 1.7, "Leve",
+				preferencia1, condiciones, null);
+		
+		Set<String> comidasGusta2= new HashSet<String>();
+		comidasGusta2.add("Carne");
+		comidasGusta2.add("pescado");
+		Set<String> comidasNoGusta2= new HashSet<String>();
+		comidasNoGusta2.add("cucaracha");
+		PreferenciaAlimenticia preferencia2=new PreferenciaAlimenticia(comidasGusta2,comidasNoGusta2);
+		Usuario usuario2 = new Usuario("Juan", "Masculino",
+				LocalDate.parse("1990-01-01"), 60.0, 1.7, "Leve",
+				preferencia2, null, null);
+		
+		Set<String> comidasGusta3= new HashSet<String>();
+		comidasGusta3.add("pescado");
+		Set<String> comidasNoGusta3= new HashSet<String>();
+		comidasNoGusta3.add("Carne");
+		PreferenciaAlimenticia preferencia3=new PreferenciaAlimenticia(comidasGusta3,comidasNoGusta3);
+		Usuario usuario3 = new Usuario("Pepo", "Masculino",
+				LocalDate.parse("1990-01-01"), 60.0, 1.7, "Leve",
+				preferencia3, null, null);
+		
+		Set<Usuario> usuariosColeccion = new HashSet<Usuario>();
+		usuariosColeccion.add(usuario1);
+		usuariosColeccion.add(usuario2);
+		usuariosColeccion.add(usuario3);
+		Set<String> comidasGusta= new HashSet<String>();
+		comidasGusta.add("Carne");
+		Set<String> comidasNoGusta= new HashSet<String>();
+		comidasNoGusta.add("lavandina");
+		PreferenciaAlimenticia preferencia=new PreferenciaAlimenticia(comidasGusta,comidasNoGusta);
+		Grupo grupo = new Grupo("UnNombre" , usuariosColeccion, preferencia);	
+
+		
+		assertFalse(grupo.puedeSugerir(receta));
+	}
+	
 }
